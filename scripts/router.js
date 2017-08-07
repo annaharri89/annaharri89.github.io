b46e2f51192c349977f8ee0d059ab9b2797be915
Anna Harrison <annaharri89@gmail.com>
2017-08-07 12:48:30 -0400

    Created router script to handle states. Created gitignore. Implemented router in index.

---
+++ scripts/router.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ETASpare on 8/7/2017.
+ */
+var myApp = angular.module('router', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+    var stitchCounterState = {
+        name: 'stitchCounter',
+        url: '/stitch_counter',
+        template: '<h3>hello world!</h3>'
+    };
+
+    var portfolioState = {
+        name: 'portfolio',
+        url: '/portfolio',
+        template: '<h3>Its the UI-Router hello world app!</h3>'
+    };
+
+    var crochetCentralState = {
+        name: 'crochetCentral',
+        url: '/crochet_central',
+        template: '<h3>Its the UI-Router hello world app!</h3>'
+    };
+
+    $stateProvider.state(stitchCounterState);
+    $stateProvider.state(portfolioState);
+    $stateProvider.state(crochetCentralState);
+});
